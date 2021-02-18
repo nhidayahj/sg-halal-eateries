ce11cb4c7817b2e9743747c471b284423d15b4ca
Hidayah <hidayah.joha@gmail.com>
2021-02-18 17:41:23 +0800

    function changes

--- load-data.js
+++ load-data.js
@@ -1,168 +1,162 @@
 async function loadData() {
-    let response = await axios.get("data.json");
-    let listOfPlaces = response.data;
-    return listOfPlaces;
+  let response = await axios.get("data.json");
+  //   let listOfPlaces = response.data;
+  let allData = response.data.moreBusinesses;
+  return allData;
 }
 
 // to only get the clean address for map API
-let clean_add = []
+let clean_add = [];
 function getCleanAdd(data) {
-    for (let i = 0; i < data.length; i++) {
-        let indexComma = data[i].addressLine1.indexOf(",")
-        if (indexComma == -1) {
-            clean_add.push({
-                'name': data[i].name,
-                'full_address': data[i].addressLine1,
-                'ratings': data[i].rating,
-                'priceRange': data[i].priceRange,
-                'categories': data[i].categories
-
-            })
-        } else if (indexComma) {
-            clean_add.push({
-                'name': data[i].name,
-                'full_address': data[i].addressLine1.slice(0, indexComma),
-                'ratings': data[i].rating,
-                'priceRange': data[i].priceRange,
-                'categories': data[i].categories
-            })
-        }
+  for (let i = 0; i < data.length; i++) {
+    let indexComma = data[i].addressLine1.indexOf(",");
+    if (indexComma == -1) {
+      clean_add.push({
+        name: data[i].name,
+        full_address: data[i].addressLine1,
+        ratings: data[i].rating,
+        priceRange: data[i].priceRange,
+        categories: data[i].categories,
+      });
+    } else if (indexComma) {
+      clean_add.push({
+        name: data[i].name,
+        full_address: data[i].addressLine1.slice(0, indexComma),
+        ratings: data[i].rating,
+        priceRange: data[i].priceRange,
+        categories: data[i].categories,
+      });
     }
-    for (let i = 0; i < clean_add.length; i++) {
-        if (clean_add[i].full_address == "2") {
-            clean_add.splice(i, 1)
-        }
+  }
+  for (let i = 0; i < clean_add.length; i++) {
+    if (clean_add[i].full_address == "2") {
+      clean_add.splice(i, 1);
     }
-    return clean_add;
+  }
+  return clean_add;
 }
 
-
-
-// getting the lat-lng for all the clean addresses 
-let newLatLngObj = []
-//update the clean add list 
+// getting the lat-lng for all the clean addresses
+let newLatLngObj = [];
+//update the clean add list
 
 async function getUpdatedList(address) {
-    let cleanAdd = getCleanAdd(address)
-    for (let i of cleanAdd) {
-        let mapUrl = `https://developers.onemap.sg/commonapi/search?searchVal=${i.full_address}&returnGeom=Y&getAddrDetails=Y`
-        let response = await axios.get(mapUrl)
-        let result = response.data
-        //console.log("result: ", result)
-        // if there is a 'found' address, get the lat-lng
-        if (result.found) {
-            newLatLngObj.push({
-                'name': i.name,
-                'full_address': result.results[0]['ADDRESS'],
-                'ratings': i.ratings,
-                'priceRange': i.priceRange,
-                'categories': i.categories,
-                'postal': result.results[0]['POSTAL'],
-                'latitude': result.results[0]['LATITUDE'],
-                'longitude': result.results[0]['LONGITUDE']
-            })
-        }
+  let cleanAdd = getCleanAdd(address);
+  for (let i of cleanAdd) {
+    let mapUrl = `https://developers.onemap.sg/commonapi/search?searchVal=${i.full_address}&returnGeom=Y&getAddrDetails=Y`;
+    let response = await axios.get(mapUrl);
+    let result = response.data;
+    //console.log("result: ", result)
+    // if there is a 'found' address, get the lat-lng
+    if (result.found) {
+      newLatLngObj.push({
+        name: i.name,
+        full_address: result.results[0]["ADDRESS"],
+        ratings: i.ratings,
+        priceRange: i.priceRange,
+        categories: i.categories,
+        postal: result.results[0]["POSTAL"],
+        latitude: result.results[0]["LATITUDE"],
+        longitude: result.results[0]["LONGITUDE"],
+      });
     }
-    //console.log("full data: ", newLatLngObj)
-    return newLatLngObj;
+  }
+  //console.log("full data: ", newLatLngObj)
+  return newLatLngObj;
 }
-
-
 
 // get the list of categories type from Clean Data to do up Modal Popup
-let cuisineArr = [];
-async function getCuisineType() {
-    let listOfPlaces = await loadData()
-    let allBusinesses = listOfPlaces.moreBusinesses
-    //    console.log(allBusinesses)
-    for (let i of allBusinesses) {
-        for (let x of i.categories) {
-            if (x.title == 'Halal') {
-                continue;
-            } else if (cuisineArr.includes(x.title)) {
-                cuisineArr;
-            } else {
-                cuisineArr.push(x.title)
-            }
-        }
+
+function getCuisineType(data) {
+  //console.log("GetCuisineType: ", data);
+  let cuisineArr = [];
+  //let listOfPlaces = await loadData();
+  //   let allBusinesses = listOfPlaces.moreBusinesses;
+  for (let i of data) {
+    for (let x of i.categories) {
+      if (x.title == "Halal") {
+        continue;
+      } else if (cuisineArr.includes(x.title)) {
+        cuisineArr;
+      } else {
+        cuisineArr.push(x.title);
+      }
     }
-    return cuisineArr;
+  }
+  //console.log(cuisineArr);
+  return cuisineArr;
 }
 
-//create the list of checkboxes in Cuisine Modal Popup 
-async function createCuisineCheckBox() {
-    let cuisineArr = await getCuisineType();
-    let dropdownOption = document.querySelector(".displayCuisine")
-    for (let i of cuisineArr) {
-        let newCheckBox = `
+//create the list of checkboxes in Cuisine Modal Popup
+function createCuisineCheckBox(data) {
+  //let cuisineArr = getCuisineType(data);
+  let dropdownOption = document.querySelector(".displayCuisine");
+  for (let i of data) {
+    let newCheckBox = `
         <input type="radio" class="cuisine" name="cuisine" value="${i}"/>
         <label>${i}</label>
         <p>
-        `
-        dropdownOption.innerHTML += newCheckBox;
-    }
+        `;
+    dropdownOption.innerHTML += newCheckBox;
+  }
 }
 
-
-// get cuisine type from user radio and return its name, add & lat-lng  
+// get cuisine type from user radio and return its name, add & lat-lng
 
 function getCuisineSelection(userSelect) {
-    let userSelectObj = [];
-    for (let i of newLatLngObj) {
-        let categories = i.categories;
-        for (let j of categories) {
-            if (j.title == userSelect) {
-                userSelectObj.push({
-                    'name': i.name,
-                    'latitude': i.latitude,
-                    'longitude': i.longitude,
-                    'ratings': i.ratings,
-                    'categories': i.categories
-                })
-            }
-        }
+  let userSelectObj = [];
+  for (let i of newLatLngObj) {
+    let categories = i.categories;
+    for (let j of categories) {
+      if (j.title == userSelect) {
+        userSelectObj.push({
+          name: i.name,
+          latitude: i.latitude,
+          longitude: i.longitude,
+          ratings: i.ratings,
+          categories: i.categories,
+        });
+      }
     }
-    console.log("User Obj: ", userSelectObj)
-    return userSelectObj;
+  }
+  console.log("User Obj: ", userSelectObj);
+  return userSelectObj;
 }
-
 
 // compare different price range list with cleanData
 function getPriceRangeLatLng(cleanData, pricelist) {
-    let priceRangeLatLng = []
-    for (let i of pricelist) {
-        for (let j of cleanData) {
-            if (j.name == i) {
-                priceRangeLatLng.push({
-                    'name': j.name,
-                    'latitude': j.latitude,
-                    'longitude': j.longitude,
-                    'ratings': j.ratings
-                })
-            }
-        }
+  let priceRangeLatLng = [];
+  for (let i of pricelist) {
+    for (let j of cleanData) {
+      if (j.name == i) {
+        priceRangeLatLng.push({
+          name: j.name,
+          latitude: j.latitude,
+          longitude: j.longitude,
+          ratings: j.ratings,
+        });
+      }
     }
-    //console.log(priceRangeLatLng)
-    return priceRangeLatLng;
+  }
+  //console.log(priceRangeLatLng)
+  return priceRangeLatLng;
 }
-
-
 
 // get the lat-lngs for ratings
 function getRatingsLatLng(cleanData, ratings) {
-    let ratingsLatLng = [];
-    for (let i of ratings) {
-        for (let j of cleanData) {
-            if (j.name == i) {
-                ratingsLatLng.push({
-                    'name': j.name,
-                    'latitude': j.latitude,
-                    'longitude': j.longitude,
-                    'ratings': j.ratings
-                })
-            }
-        }
+  let ratingsLatLng = [];
+  for (let i of ratings) {
+    for (let j of cleanData) {
+      if (j.name == i) {
+        ratingsLatLng.push({
+          name: j.name,
+          latitude: j.latitude,
+          longitude: j.longitude,
+          ratings: j.ratings,
+        });
+      }
     }
-    return ratingsLatLng;
-    //console.log("Ratings Lat-Lng: " , ratingsLatLng)
+  }
+  return ratingsLatLng;
+  //console.log("Ratings Lat-Lng: " , ratingsLatLng)
 }
--- script.js
+++ script.js
@@ -1,135 +1,114 @@
-let findBtn = document.querySelector("#find-btn")
-findBtn.addEventListener('click', async () => {
-    let searchValue = document.querySelector("#search-bar").value;
-    console.log(searchValue)
+let findBtn = document.querySelector("#find-btn");
+findBtn.addEventListener("click", async () => {
+  let searchValue = document.querySelector("#search-bar").value;
+  console.log(searchValue);
+});
 
-})
+let listOfPlaces;
+window.addEventListener("DOMContentLoaded", async () => {
+  //let allData = await loadData();
+  //let listOfPlaces = allData.moreBusinesses;
+  listOfPlaces = await loadData();
+  //raw data info:
+  console.log("All the list: ", listOfPlaces);
+  //filtered data:
+  let cleanFullData = await getUpdatedList(listOfPlaces);
+  console.log("clean list: ", cleanFullData);
 
-window.addEventListener('DOMContentLoaded', async () => {
-    let allData = await loadData()
-    let listOfPlaces = allData.moreBusinesses;
-    //raw data info:
-    console.log("All the list: ", listOfPlaces);
-    //filtered data:
-    let cleanFullData = await getUpdatedList(listOfPlaces)
-    console.log("clean list: ", cleanFullData)
+  //load map with ALL the markers at initial load page
 
-    //load map with ALL the markers at initial load page
-    
+  let cuisineArr = getCuisineType(cleanFullData);
+  console.log("all cuisine:", cuisineArr);
 
-    let cuisineArr = await getCuisineType()
-    //console.log("all cuisine:" ,cuisineArr)
+  // display cuisine checkbox in Cuisine filter
+  createCuisineCheckBox(cuisineArr);
+  //store user's cuisine selection
 
-    // display cuisine checkbox in Cuisine filter 
-    await createCuisineCheckBox();
-    //store user's cuisine selection
-    
-    let cuisineFilterSelectBtn = document.querySelector("#cuisineFilterSuccessBtn")
-    cuisineFilterSelectBtn.addEventListener('click', () => {
-        let userCuisineSelection;
-        let userSelect;
-        let cuisineSelection = document.querySelectorAll(".cuisine")
-        for (let i of cuisineSelection) {
-            if (i.checked) {
-                console.log("User selected: " , i.value)
-                userSelect = getCuisineSelection(i.value);
-            }
-        }
-        addCuisineLayers(cuisineLayer, userSelect)
-    })
-
-    
-
-
-    // categorize data by their price range
-    let priceList = {
-        'affordable': [],
-        'mid_range': [],
-        'expensive': []
+  let cuisineFilterSelectBtn = document.querySelector(
+    "#cuisineFilterSuccessBtn"
+  );
+  cuisineFilterSelectBtn.addEventListener("click", () => {
+    let userCuisineSelection;
+    let userSelect;
+    let cuisineSelection = document.querySelectorAll(".cuisine");
+    for (let i of cuisineSelection) {
+      if (i.checked) {
+        console.log("User selected: ", i.value);
+        userSelect = getCuisineSelection(i.value);
+      }
     }
+    addCuisineLayers(cuisineLayer, userSelect);
+  });
 
-    for (let i of cleanFullData) {
-        if (i.priceRange) {
-            if (i.priceRange == 1) {
-                priceList['affordable'].push(i.name);
-            } else if (i.priceRange == 2) {
-                priceList['mid_range'].push(i.name);
-            } else if (i.priceRange >= 3) {
-                priceList['expensive'].push(i.name);
-            }
-        }
+  // categorize data by their price range
+  let priceList = {
+    affordable: [],
+    mid_range: [],
+    expensive: [],
+  };
+
+  for (let i of cleanFullData) {
+    if (i.priceRange) {
+      if (i.priceRange == 1) {
+        priceList["affordable"].push(i.name);
+      } else if (i.priceRange == 2) {
+        priceList["mid_range"].push(i.name);
+      } else if (i.priceRange >= 3) {
+        priceList["expensive"].push(i.name);
+      }
     }
-    // 13-affordable, 11-mid_range, 0-exp
-    //console.log("List of price Range: ", priceList)
+  }
+  // 13-affordable, 11-mid_range, 0-exp
+  //console.log("List of price Range: ", priceList)
 
-
-
-    // categorize data by ratings range
-    let ratingsList = {
-        'low': [],
-        'medium': [],
-        'high': []
+  // categorize data by ratings range
+  let ratingsList = {
+    low: [],
+    medium: [],
+    high: [],
+  };
+  for (let i of cleanFullData) {
+    if (i.ratings < 3) {
+      ratingsList["low"].push(i.name);
+    } else if (i.ratings <= 4) {
+      ratingsList["medium"].push(i.name);
+    } else if (i.ratings <= 5) {
+      ratingsList["high"].push(i.name);
     }
-    for (let i of cleanFullData) {
-        if (i.ratings < 3) {
-            ratingsList['low'].push(i.name)
-        } else if (i.ratings <= 4) {
-            ratingsList['medium'].push(i.name)
-        } else if (i.ratings <= 5) {
-            ratingsList['high'].push(i.name)
-        }
-    }
-    //console.log(ratingsList)
+  }
+  //console.log(ratingsList)
 
+  checksExistingLayer(currentLayer, cleanFullData);
 
+  document.querySelector("#afford").addEventListener("click", function () {
+    let affordPriceList = getPriceRangeLatLng(
+      cleanFullData,
+      priceList["affordable"]
+    );
+    //checks if there is an existing previous layer on map
+    checksExistingLayer(currentLayer, affordPriceList);
+  });
 
-    checksExistingLayer(currentLayer, cleanFullData)
+  document.querySelector("#mid_range").addEventListener("click", function () {
+    let midPriceList = getPriceRangeLatLng(
+      cleanFullData,
+      priceList["mid_range"]
+    );
+    checksExistingLayer(currentLayer, midPriceList);
+  });
 
-    document.querySelector("#afford").addEventListener('click', function () {
-        
-        let affordPriceList = getPriceRangeLatLng(cleanFullData, priceList['affordable'])
-        //checks if there is an existing previous layer on map
-        checksExistingLayer(currentLayer, affordPriceList)
-    })
+  document.getElementById("low").addEventListener("click", function () {
+    let lowRatings = getRatingsLatLng(cleanFullData, ratingsList["low"]);
+    checksExistingLayer(currentLayer, lowRatings);
+  });
 
-    document.querySelector("#mid_range").addEventListener('click', function () {
+  document.getElementById("medium").addEventListener("click", function () {
+    let medRatings = getRatingsLatLng(cleanFullData, ratingsList["medium"]);
+    checksExistingLayer(currentLayer, medRatings);
+  });
 
-        let midPriceList = getPriceRangeLatLng(cleanFullData, priceList['mid_range'])
-        checksExistingLayer(currentLayer, midPriceList)
-    })
-
-
-    document.getElementById('low').addEventListener('click', function () {
-        
-        let lowRatings = getRatingsLatLng(cleanFullData, ratingsList['low'])
-        checksExistingLayer(currentLayer, lowRatings)
-    })
-
-    document.getElementById('medium').addEventListener('click', function () {
-        
-        let medRatings = getRatingsLatLng(cleanFullData, ratingsList['medium'])
-        checksExistingLayer(currentLayer, medRatings)
-
-    })
-
-    document.getElementById('high').addEventListener('click', function () {
-
-        let highRatings = getRatingsLatLng(cleanFullData, ratingsList['high'])
-        checksExistingLayer(currentLayer, highRatings)
-
-    })
-
-
-
-})
-
-
-
-
-
-
-
-
-
-
-
+  document.getElementById("high").addEventListener("click", function () {
+    let highRatings = getRatingsLatLng(cleanFullData, ratingsList["high"]);
+    checksExistingLayer(currentLayer, highRatings);
+  });
+});
